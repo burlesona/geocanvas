fd5ac1d6fae36f059d95736edef1475f130d1111
Andrew Burleson <burlesona@gmail.com>
2012-08-25 20:28:11 -0500

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'thin'
+
+gem 'haml-rails'
+gem 'jquery-rails'
+gem 'jetpack-rails', :git => 'git://github.com/burlesona/jetpack-rails.git'
+gem 'simple_form'
+gem 'rdiscount'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+end
+
+group :development, :test do
+	gem 'sqlite3'
+	gem 'minitest-rails'
+	gem 'fabrication'
+	gem 'guard-minitest'
+end
+
+group :production do
+	gem 'pg'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geocanvas::Application.config.secret_token = '2b507c56dc1c3fdc5eecf4809f63ee3b5fd07d049033f4f662310efda48ad35167667e9d4dbc3889d08bf5687364b53d08c9a0328dfdf984a72d7f4fe2b2d077'
